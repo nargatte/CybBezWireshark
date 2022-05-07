95c64f08013fef7b99ce433d6a2b82727ddef254
Grzegorz Krzysiak <krzysiak.grzegorz@gmail.com>
2022-05-08 00:13:21 +0200

    add appsettings.json + coppy build event

--- CybBezWireshark/Form1.cs
+++ CybBezWireshark/Form1.cs
@@ -1,10 +1,15 @@
+using Microsoft.Extensions.Configuration;
+
 namespace CybBezWireshark
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        private readonly IConfiguration config;
+
+        public Form1(IConfiguration config)
         {
             InitializeComponent();
+            this.config = config;
         }
 
         private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
--- CybBezWireshark/Program.cs
+++ CybBezWireshark/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 namespace CybBezWireshark
 {
     internal static class Program
@@ -6,12 +10,15 @@ namespace CybBezWireshark
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
+            using IHost host = Host.CreateDefaultBuilder(args).Build();
+            IConfiguration config = host.Services.GetRequiredService<IConfiguration>();
+
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new Form1(config));
         }
     }
 }
